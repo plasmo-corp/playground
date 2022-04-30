1e124e7408b0d1255c42bc8b7a9258344c8e5e7f
Denpti Plasmo <denpti.plasmo@denpti.com>
2022-04-29 20:08:50 -0400

    Add steps to npm generator

--- src/generators/npm.ts
+++ src/generators/npm.ts
@@ -5,7 +5,18 @@ import type { Repo } from "~common/repo";
 const generator: StepsGenerator = {
   name: 'npm',
   fn: (repo: Repo) => {
-    return []
+    return [
+      {uses: "actions/checkout@v2"},
+      {run: "yarn install"},
+      { run: "yarn build"},
+      { name: "Browser Platform Publish" },
+      { uses: "plasmo-corp/bpp@v2" },
+      {
+        with: {
+          keys: "${{ secrets.SUBMIT_KEYS }}"
+        }
+      }
+    ]
   }
 }
 
