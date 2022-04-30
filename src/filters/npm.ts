b6f0acf76bda92ac27093c6514c70541bfef5a29
Denpti Plasmo <denpti.plasmo@denpti.com>
2022-04-29 20:08:26 -0400

    Adjust coding style

--- src/filters/npm.ts
+++ src/filters/npm.ts
@@ -1,20 +1,21 @@
 import type { RepoFilter } from "./index";
 import type { Repo } from "~common/repo";
 
-var SEARCH_TERMS = ['eslint', "build", "lint"];
+const SEARCH_TERMS = ['eslint', "build", "lint"];
 
 const Filter: RepoFilter = {
   name: 'npm',
   searchTerms: SEARCH_TERMS,
   fn: (repo: Repo) => {
     // Look through search results. If identify package.json file then big points
-    var searchResults = repo.search.getResultsFor(SEARCH_TERMS);
 
-    for (let index = 0; index < SEARCH_TERMS.length; index++) {
-      const term = SEARCH_TERMS[index];
+    // var searchResults = repo.search.getResultsFor(SEARCH_TERMS);
+
+    // for (let index = 0; index < SEARCH_TERMS.length; index++) {
+    //   const term = SEARCH_TERMS[index];
       
-      console.log("For npm search term: ", term, " got result: ", searchResults[index]);
-    }
+    //   console.log("For npm search term: ", term, " got result: ", searchResults[index]);
+    // }
 
     // TODO: actually do something meaningful with the results
     return ['package.json']
